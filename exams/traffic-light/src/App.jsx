825d7d1810c04a8b1887274ec360c3569bc102b4
jlimsy <limsy.joey@gmail.com>
2024-04-22 13:28:47 +0800

    change screen breakpoint from lg to md

--- exams/traffic-light/src/App.jsx
+++ exams/traffic-light/src/App.jsx
@@ -2,7 +2,7 @@ import TrafficLight from "./TrafficLight";
 
 export default function App() {
   return (
-    <div className="h-screen lg:bg-gradient-to-b lg:from-sky lg:to-white bg-gradient-to-b from-sky/80 to-black">
+    <div className="h-screen md:bg-gradient-to-b md:from-sky md:to-white bg-gradient-to-b from-sky/80 to-black">
       <div className="absolute text-center transform -translate-x-1/2 -translate-y-1/2 border rounded-xl top-1/2 left-1/2 overflow-hidden">
         <TrafficLight />
       </div>
--- exams/traffic-light/src/TrafficLight.jsx
+++ exams/traffic-light/src/TrafficLight.jsx
@@ -48,22 +48,22 @@ export default function TrafficLight() {
       <div
         className={
           green
-            ? "lg:w-24 lg:h-24 w-16 h-16 bg-green rounded-full"
-            : "lg:w-24 lg:h-24 w-16 h-16 bg-inactive rounded-full"
+            ? "md:w-24 md:h-24 w-16 h-16 bg-green rounded-full"
+            : "md:w-24 md:h-24 w-16 h-16 bg-inactive rounded-full"
         }
       ></div>
       <div
         className={
           yellow
-            ? "lg:w-24 lg:h-24 w-16 h-16 bg-yellow rounded-full"
-            : "lg:w-24 lg:h-24 w-16 h-16 bg-inactive rounded-full"
+            ? "md:w-24 md:h-24 w-16 h-16 bg-yellow rounded-full"
+            : "md:w-24 md:h-24 w-16 h-16 bg-inactive rounded-full"
         }
       ></div>
       <div
         className={
           red
-            ? "lg:w-24 lg:h-24 w-16 h-16 bg-red rounded-full"
-            : "lg:w-24 lg:h-24 w-16 h-16 bg-inactive rounded-full"
+            ? "md:w-24 md:h-24 w-16 h-16 bg-red rounded-full"
+            : "md:w-24 md:h-24 w-16 h-16 bg-inactive rounded-full"
         }
       ></div>
     </div>
